2c623da12192b0f1899c7cc7ae33dac0a2e3e4d4
TahaniKarroum <tahani.karroum@gmail.com>
2016-09-28 22:24:58 +0300

    Add new function in produc model to get products by category
    Add new function in produc model to get products by category  + fix api
    controller bcz i have an error in json

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -5,19 +5,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import models.Category;
-
+import models.Product;
+import net.sf.json.JSONArray;
 
 public class Api extends controllers.CRUD {
-	
-	public static String getAllCategories(){
-		Gson gson=new Gson();
-		List<Category> allCategoriesList=Category.allMenuCategories().fetch();
+
+	public static JSONArray getAllCategories() {
+		Gson gson = new Gson();
+		List<Category> allCategoriesList = Category.allMenuCategories().fetch();
 		String jsonData = gson.toJson(allCategoriesList);
-		renderText(jsonData);
-		return jsonData;
+		JSONArray jsonA = JSONArray.fromObject(jsonData);
+		return jsonA;
 	}
+
+	public static JSONArray getAllProducts(String categoryid) {
+		Gson gson = new Gson();
+		List<Product> allProducts = Product.getProductByCategory(categoryid).fetch();
+		String jsonData = gson.toJson(allProducts);
+		JSONArray jsonA = JSONArray.fromObject(jsonData);
+		return jsonA;
+	}
+
 }
