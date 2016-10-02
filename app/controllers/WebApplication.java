e34b61b10a48b8bbc0d2113f697bcf4d80c1bec6
Alaa Deiry <alaa.deiry@gmail.com>
2016-10-02 19:58:30 +0300

    Get food categories and display them in the website

--- app/controllers/WebApplication.java
+++ app/controllers/WebApplication.java
@@ -3,6 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import siena.Model;
+import utils.Pagination;
 import utils.Enums.LogType;
 
 import java.security.MessageDigest;
@@ -15,7 +16,30 @@ import models.*;
 public class WebApplication extends Controller {
 
     public static void index() {
+    	System.out.println("Website");
         render();
+    }
+    
+    public static void menu() {
+    	System.out.println("Menu section");
+    	int menuCategoriesCount = Category.allMenuCategories().count(); 	
+    	System.out.println("menu categories count"+menuCategoriesCount);
+		List<Category> foodMenuCategories = null;
+		if(menuCategoriesCount > 0){
+			foodMenuCategories = Category.allMenuCategories().fetch();
+		}
+		render(foodMenuCategories);
+	}
+    
+    public List<Category> foodMenu(){
+    	System.out.println("Menu section");
+    	int menuCategoriesCount = Category.allMenuCategories().count(); 	
+    	System.out.println("menu categories count"+menuCategoriesCount);
+		List<Category> foodMenuCategories = null;
+		if(menuCategoriesCount > 0){
+			foodMenuCategories = Category.allMenuCategories().fetch();
+		}
+		return foodMenuCategories;
     }
 
 
