b58b0200c57cd7e3a4d4a3ca79481dee0b9b0645
Tarek Adra <adra.tarek@gmail.com>
2016-12-03 10:24:04 +0200

    Model Delivery Updated

--- app/models/Delivery.java
+++ app/models/Delivery.java
@@ -17,29 +17,17 @@ public class Delivery extends Model {
 	@Id(Generator.UUID)
 	public String ID;
 	
-	public String first_name;
-	public String last_name;
-	public String phone;
-	public String state;
-	public String guest_number;
-	public String email;
-	public String subject;
-	public String res_status;//pending accepted canceled
-	public String res_client_id;
-	public int res_numb_pers;
-	public Date res_date;
-
-	public String res_date_from;
-	public String res_date_to;
+	public String ClientID;
+	public String DeliveryStatus;
+	public String ClientLocation;
+	public String Notes;  
+	public String DeliveryRating;
 
 
 	@Override
 	public String toString() {
-		return "Reservation [res_id=" + res_id + ", first_name=" + first_name + ", phone=" + phone + ", last_name="
-				+ last_name + ", state=" + state + ", guest_number=" + guest_number + ", email=" + email + ", subject="
-				+ subject + ", res_status=" + res_status + ", res_client_id=" + res_client_id + ", res_numb_pers="
-				+ res_numb_pers + ", res_date=" + res_date + ", res_date_from=" + res_date_from + ", res_date_to="
-				+ res_date_to + "]";
+		return "Delivery [ID=" + ID + ", ClientID=" + ClientID + ", DeliveryStatus=" + DeliveryStatus
+				+ ", ClientLocation=" + ClientLocation + ", Notes=" + Notes + "]";
 	}
 
 	public static Query<Delivery> all() {
@@ -57,8 +45,8 @@ public class Delivery extends Model {
 
 	public void saveReservation() {
 
-		if (res_id == null || res_id.equals("") == true) {
-			this.res_id = null;
+		if (ID == null || ID.equals("") == true) {
+			this.ID = null;
 
 			Model.batch(Delivery.class).insert(this);
 		} else {
